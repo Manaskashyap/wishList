135c40fa3cc9e7f1ed460f3d99c9ee3cef90ee64
Syed Masood <masoodsyed123786@gmail.com>
2023-08-23 17:59:41 +0530

    add mongodb acees

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://manaswishlist:manaswishlist123@cluster0.25dnfqa.mongodb.net/";
+const CONNECTION_URL = "mongodb+srv://Syed:123@atlascluster.t71vqzg.mongodb.net/";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
